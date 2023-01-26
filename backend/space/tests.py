e54ab9ed70b19774de4a910f64bf0198aa20a192
fero <mohit.talreja@fero.ai>
2023-01-26 18:17:32 +0530

    More test cases

--- backend/space/tests.py
+++ backend/space/tests.py
@@ -28,3 +28,33 @@ class SpaceViewSetTest(test.APITestCase):
     def test_space_list_successful_response_code(self):
         response = self.client.get(self.base_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_space_detail_for_valid_id(self):
+        space_id = 2
+        detail_url = f"{self.base_url}{space_id}/"
+        response = self.client.get(path=detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_space_detail_for_invalid_id(self):
+        space_id = 10
+        detail_url = f"{self.base_url}{space_id}/"
+        response = self.client.get(path=detail_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_space_create_no_data(self):
+        data = {}
+        response = self.client.post(path=self.base_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_space_create_with_existing_name(self):
+        data = {"name": "Space-2", "total_slots": 2}
+        response = self.client.post(self.base_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_space_create_with_valid_data(self):
+        data = {"name": "Space-6", "total_slots": 2}
+        count_before_create = Space.objects.count()
+        response = self.client.post(self.base_url, data=data)
+        count_after_create = Space.objects.count()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(count_after_create, (count_before_create+1))
