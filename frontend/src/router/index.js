9d40591341aec9dff1ba2661b5098df7cf9bcd66
fero <mohit.talreja@fero.ai>
2023-02-22 19:05:33 +0530

    added navigation guard structure

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,12 +4,18 @@ import Default from "@/layout/Default.vue";
 import PageNotFound from "@/components/Common/PageNotFound";
 import HomeView from "../views/admin/HomeView.vue";
 import BookingPage from "@/views/admin/BookingPage";
+import Register from "@/views/admin/Register";
 
 const routes = [
   {
     path: "/",
     name: "login",
     component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
   },
   {
     path: "/app",
@@ -20,11 +26,11 @@ const routes = [
         name: "home",
         component: HomeView,
       },
-      {
-        path: "home",
-        name: "home",
-        component: HomeView,
-      },
+      // {
+      //   path: "home",
+      //   name: "home",
+      //   component: HomeView,
+      // },
       {
         path: "book",
         name: "BookingPage",
@@ -43,6 +49,10 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  next(); // next is function which is required to be called
 });
 
 // Use navigation guard for preventing access of non-super users router.beforeEach
