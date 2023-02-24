f2cb53cba6984992f374830ff07bbc3dcfbedf9c
fero <mohit.talreja@fero.ai>
2023-02-24 19:32:05 +0530

    Removed unncessary models

--- backend/account_management/admin.py
+++ backend/account_management/admin.py
@@ -1,21 +1,9 @@
 from django.contrib import admin
 
-from .models import Account, NormalUser, CareTakerUser
+from .models import Account
 
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = ('id', 'account_type', 'phone_number', 'user')
-    list_filter = ('user',)
-
-
-@admin.register(NormalUser)
-class NormalUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'has_premium', 'premium_expiry')
-    list_filter = ('user', 'has_premium', 'premium_expiry')
-
-
-@admin.register(CareTakerUser)
-class CareTakerUserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user')
     list_filter = ('user',)
--- backend/account_management/serializers.py
+++ backend/account_management/serializers.py
@@ -5,42 +5,6 @@ from django.utils import timezone
 from account_management import utils, models
 from lib import constants
 
-
-class NormalUserProfileSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(source="user.first_name")
-    last_name = serializers.CharField(source="user.last_name")
-    username = serializers.CharField(source="user.username")
-    password = serializers.CharField(source="user.password", write_only=True)
-
-    class Meta:
-        model = models.NormalUser
-        fields = (
-            "first_name", "last_name", "username", "has_premium", "password",
-        )
-
-    def validate_username(self, username):
-        if User.objects.filter(username__iexact=username).exists():
-            raise serializers.ValidationError(f"User with username {username} is already registered."
-                                              f"Please choose different username")
-        return username
-
-    def validate(self, attrs):  # Move to field validation
-        premium_expiry = attrs.get("premium_expiry", None)
-        if premium_expiry and premium_expiry <= timezone.now():
-            raise serializers.ValidationError({"premium_expiry": "Premium Expiry cannot be in Past"})
-
-        attrs["has_premium"] = True
-        return super(NormalUserProfileSerializer, self).validate(attrs=attrs)
-
-    def create(self, validated_data):
-        user = validated_data.pop("user")
-        password = user.pop("password")
-        user: User = User.objects.create_user(**user, password=password)
-        # user.set_password(raw_password=password)
-        validated_data["user"] = user
-        return super(NormalUserProfileSerializer, self).create(validated_data=validated_data)
-
-
 class UserProfileSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(source="user.first_name")
     last_name = serializers.CharField(source="user.last_name")
